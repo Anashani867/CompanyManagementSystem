b2bf59a7d584631a0adbecf16fbb93513e306074
Anas Hassan <anashassan.it@gmail.com>
2025-07-29 10:45:13 +0300

    Update Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -158,7 +158,7 @@ if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 
 
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
