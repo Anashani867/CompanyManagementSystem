1097b5de6dd417f1e8bd67ae2bfed91049aae678
Anas Hassan <anashassan.it@gmail.com>
2025-07-29 11:51:46 +0300

    fix/nullable/EmployeeProject.cs

--- Core/Entities/EmployeeProject/EmployeeProject.cs
+++ Core/Entities/EmployeeProject/EmployeeProject.cs
@@ -2,12 +2,11 @@ namespace Core.Entities
 {
     public class EmployeeProject
     {
+        //public int Id { get; set; }
+        public Employee Employee { get; set; } = null!;
         public int EmployeeId { get; set; }
-        public Employee Employee { get; set; }
-
+        public Project Project { get; set; } = null!;
         public int ProjectId { get; set; }
-        public Project Project { get; set; }
 
         public int AssignedHours { get; set; }
     }
-}
