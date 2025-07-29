dbf48a10285fbcd7ec65f255cef367e626fef1a0
Anas Hassan <anashassan.it@gmail.com>
2025-07-29 11:49:28 +0300

    fix/nullable/Employee.cs

--- Core/Entities/Employee/Employee.cs
+++ Core/Entities/Employee/Employee.cs
@@ -3,10 +3,8 @@ namespace Core.Entities
     public class Employee
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public bool IsDeleted { get; set; } = false;
-
-        // Navigation property for many-to-many
-        public ICollection<EmployeeProject> EmployeeProjects { get; set; }
+        public ICollection<EmployeeProject> EmployeeProjects { get; set; } = new List<EmployeeProject>();
     }
 }
