f09a4c8c85b72e5ec2a8634f756cad8aab415de0
Anas Hassan <anashassan.it@gmail.com>
2025-07-29 11:50:51 +0300

    fix/nullable/Project.cs

--- Core/Entities/Project/Project.cs
+++ Core/Entities/Project/Project.cs
@@ -3,11 +3,9 @@ namespace Core.Entities
     public class Project
     {
         public int Id { get; set; }
-        public string? Title { get; set; }
+        public string? Title { get; set; } = string.Empty;
         public bool IsDeleted { get; set; } = false;
-
-
         // Navigation property for many-to-many
-        public ICollection<EmployeeProject> EmployeeProjects { get; set; }
+        public ICollection<EmployeeProject> EmployeeProjects { get; set; } = new List<EmployeeProject>();
     }
 }
