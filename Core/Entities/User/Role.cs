2773fd2bf164c948ae3d0eac479702214a54c2d4
Anas Hassan <anashassan.it@gmail.com>
2025-07-29 11:46:37 +0300

    fix/nullable Role.cs

--- Core/Entities/User/Role.cs
+++ Core/Entities/User/Role.cs
@@ -3,10 +3,8 @@ namespace Core.Entities
     public class Role
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-
-        public ICollection<UserRole> UserRoles { get; set; }
-
+        public string Name { get; set; } = string.Empty;
+        public ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
         //public ICollection<User> Users { get; set; }
     }
 
